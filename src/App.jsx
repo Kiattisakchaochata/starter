1b1cc7fbc5821faf927749d557527feda8d63f6f
kiattisakchaochata <kiattisakchaochata@gmail.com>
2025-05-16 09:20:45 +0700

    workshop

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+   <div className="app">
+    <h1 className="text-3xl text-amber-300">Codecamp20</h1>
+    <button className="btn btn-primary">Ok</button>
+   </div>
+  )
+}
+
+export default App
